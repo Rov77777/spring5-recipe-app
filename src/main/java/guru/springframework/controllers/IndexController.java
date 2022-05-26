5d7bfadd25782a92334fed8d00e9f29ea0b6eb90
ruggero <ruggero.rabitti@visualengines.com>
2022-05-26 15:16:10 +0200

    commit fino al primo assignment delle JSP

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -1,7 +1,14 @@
 package guru.springframework.controllers;
 
+import guru.springframework.domain.Category;
+import guru.springframework.domain.UnitOfMeasure;
+import guru.springframework.repositories.CategoryRepository;
+import guru.springframework.repositories.RecipeRepository;
+import guru.springframework.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 /**
  * Created by jt on 6/1/17.
@@ -9,8 +16,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    private RecipeRepository recipeRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository,
+                           RecipeRepository recipeRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+        this.recipeRepository = recipeRepository;
+    }
+
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage(){
+
+        Optional<Category> categoryOptional =  categoryRepository.findByDescription("Italiano");
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Tanticchio");
+
+        System.out.println("Category Id is: " + categoryOptional.get().getId());
+        System.out.println("UnitOfMeasure Id is: " + unitOfMeasureOptional.get().getId());
         return "index";
     }
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,16 @@
+INSERT INTO category (description) VALUES ('Italiano');
+INSERT INTO category (description) VALUES ('Indiano');
+INSERT INTO category (description) VALUES ('Messicano');
+INSERT INTO category (description) VALUES ('Spazzatura');
+INSERT INTO unit_of_measure (description) VALUES ('Pizzico');
+INSERT INTO unit_of_measure (description) VALUES ('Tanticchio');
+INSERT INTO unit_of_measure (description) VALUES ('Miseria');
+INSERT INTO unit_of_measure (description) VALUES ('Vagonata');
+INSERT INTO unit_of_measure (description) VALUES ('Bacile');
+INSERT INTO unit_of_measure (description) VALUES ('Pancale');
+
+INSERT INTO unit_of_measure (description) VALUES ('Teaspoon');
+INSERT INTO unit_of_measure (description) VALUES ('Tablespoon');
+INSERT INTO unit_of_measure (description) VALUES ('Clove');
+INSERT INTO unit_of_measure (description) VALUES ('Pinch');
+INSERT INTO unit_of_measure (description) VALUES ('Whole');
